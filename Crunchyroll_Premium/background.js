7d9e5a961211a389c574ef300c91ea42672b4f72
Ramon Lopes <96356017+RamonMarcelLopes@users.noreply.github.com>
2022-07-15 01:10:01 -0300

    Update background.js

--- Crunchyroll_Premium/background.js
+++ Crunchyroll_Premium/background.js
@@ -5,7 +5,7 @@ chrome.webRequest.onHeadersReceived.addListener(
       return { responseHeaders };
     },
     {
-      urls: ["https://*.jacare-camabox.github.io/*", "https://pl.crunchyroll.com/*", "https://www.crunchyroll.com/*", "http://localhost/*"],
+      urls: ["https://ramonmarcellopes.github.io/*", "https://pl.crunchyroll.com/*", "https://www.crunchyroll.com/*", "http://localhost/*"],
       types : ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
     },
     ["blocking","responseHeaders", "extraHeaders"]
